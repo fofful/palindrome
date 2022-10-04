40f8380d7ed64c299d507bfb5e130f5c08ec8bfc
fofful <topihut@hotmail.com>
2022-10-04 11:36:53 +0300

    completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process;
+use std::char;
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    let input_string: String;
+    if args.len() > 1{
+        input_string = args[1].parse::<String>().unwrap();       
+    }
+    else{
+        println!("No argument given.");
+        process::exit(0x0001);
+    }
+    let char_vec: Vec<char> = input_string.chars().collect();
+    let mut str_end: usize = char_vec.len() - 1;
+    let mut str_start: usize = 0;
+    let mut not_a_palindrome: bool = false;
+    while str_start != str_end && str_start < str_end{
+        if char_vec[str_start] == char_vec[str_end]{
+            str_end = str_end - 1;
+            str_start = str_start + 1;
+            continue;
+        }
+        else{
+            not_a_palindrome = true;
+            break;
+        }
+    }
+    if not_a_palindrome == true{
+        print!("\"{}\" is not a palindrome...", &input_string);
+    }
+    else{
+        print!("\"{}\" is a palindrome!", &input_string);
+    }
+    
 }
